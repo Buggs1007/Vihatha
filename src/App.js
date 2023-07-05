0f794d01bf49ca99433ba4747eff0a95ce8d8c95
lakshyavijay29 <lakshyavijay34@gmail.com>
2023-07-05 16:32:52 +0530

    Navbar edits -01

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import NavBar from './NavbarComponents/Navbar';
+import "./NavbarComponents/Navbar.css"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <NavBar />
       </header>
     </div>
   );
---
+++ src/NavbarComponents/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul className="nav-links">
+        <li>
+          <a href=""><h2>Home</h2></a>
+        </li>
+        <li>
+          <a href=""><h2>About Us</h2></a>
+        </li>
+        <li>
+          <a href=""><h2>Programs</h2></a>
+        </li>
+        <li>
+          <a href=""><h2>Collaborate</h2></a>
+        </li>
+        <li>
+          <a href=""><h2>Support</h2></a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
