7c9541af7d8f8a38e2078d8e4dda35f6e48e8286
Adi-2310 <aditya234512@gmail.com>
2023-07-24 00:20:16 +0530

    blog-six-changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import BlogFour from './BlogFour/BlogFour';
 import BlogFive from './BlogFive/BlogFive';
+import BlogSix from './BlogSix/BlogSix';
 
 function App() {
   return (
     <div >
       {/* <BlogFour/> */}
-      <BlogFive/>
+      {/* <BlogFive/> */}
+      <BlogSix/>
     </div>
   );
 }
