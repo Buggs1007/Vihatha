77d4cfb8390a7d353bfb4be8c96bbfaac17a34ed
jain-lakshit <82159394+jain-lakshit@users.noreply.github.com>
2023-07-26 10:41:38 +0530

    resolving conflicts to rebase to main

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-// import OurStory from './components/OurStory/OurStory';
-// import VisionPage from './components/VisionPage/VisionPage';
-// import KeyFocusAreas from './components/KeyFocusAreas/KeyFocusAreas';
-// import Support from './components/Support/Support';
-// import OurTeam from './components/OurTeam/OurTeam';
-import MedhaswiProg from './components/MedhaswiProg/MedhaswiProg';
-// import SelpProgram from './components/SelpProgram/SelpProgram';
-// import WellBeingProg from './components/WellBeingProg/WellBeingProg';
-// import BlogTwo from './components/BlogTwo/BlogTwo';
-// import BlogOne from './components/BlogOne/BlogOne';
-// import BlogThree from './components/BlogThree/BlogThree';
-// import BlogFourth from './components/BlogFourth/BlogFourth';
+import './App.css';
+import Home from './Homepage/Homepage.js';
+import NavBar from './NavbarComponents/Navbar';
+import "./NavbarComponents/Navbar.css"
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <Navbar />
-      {/* <VisionPage /> */}
-      {/* <Support /> */}
-      {/* <OurStory /> */}
-      {/* <OurTeam />     */}
-      {/* <KeyFocusAreas/> */}
-      {/* <WellBeingProg/> */}
-      {/* <SelpProgram /> */}
-      {/* <BlogTwo /> */}
-      {/* <BlogOne /> */}
-      {/* <BlogThree /> */}
-      {/* <BlogFourth /> */}
-      <MedhaswiProg />
+    <div className="App">
+    <NavBar />
+    <Home />
+      <header className="App-header">
       
-     
+      </header>
     </div>
   );
-};
+}
 
 export default App;
