648b18ffe1d122aac62274c3d4f7037c5cc0ab45
jain-lakshit <82159394+jain-lakshit@users.noreply.github.com>
2023-07-26 15:23:31 +0530

    updating file structuring

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import './App.css';
-import BlogFour from './BlogFour/BlogFour';
-import BlogFive from './BlogFive/BlogFive';
-import BlogSix from './BlogSix/BlogSix';
-import BharatYatra from './BharatYatra/Bharatyatra';
-import NaniProgram from './NaniProgram/NaniProgram';
-import KitchenProgram from './KitchenProgram/KitchenProgram';
+import Home from './Homepage/Homepage.js';
+import NavBar from './NavbarComponents/Navbar';
+import "./NavbarComponents/Navbar.css"
 
 function App() {
   return (
-    <div >
-      {/* <BlogFour/> */}
-      {/* <BlogFive/> */}
-      {/* <BlogSix/> */}
-      {/* <BharatYatra/> */}
-    {/* <KitchenProgram/> */}
-    <NaniProgram/>
+    <div className="App">
+    <NavBar />
+    <Home />
+      <header className="App-header">
+      
+      </header>
     </div>
   );
 }
